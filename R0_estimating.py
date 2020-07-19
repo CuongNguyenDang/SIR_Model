aa1357855b6afa3363fba64db0dd54e9d77ffa92
= <=>
2020-07-20 02:26:50 +0700

    fix sampling.py

---
+++ R0_estimating.py
@@ -0,0 +1,6 @@
+import numpy as np
+import scipy.stats as st
+import seaborn as sns
+from matplotlib import pyplot as plt
+import math
+import pylab
--- sampling.py
+++ sampling.py
@@ -2,8 +2,6 @@ import numpy as np
 import scipy.stats as st
 import seaborn as sns
 from matplotlib import pyplot as plt
-
-
 mus = np.array([0, 0])
 
 
@@ -16,12 +14,14 @@ def pgauss(x, y):
     return st.multivariate_normal.pdf([x, y], mean=mus)
 
 
-def metropolis_hastings(p, iter=1000):
-    beta, gamma = 0., 0.
+def metropolis_hastings(p, iter):
+    beta, gamma = 0.0212, 3.39 #The starting point of our Markov chain is the estimated value of (α, β) from Raggett (1982)?????
     samples = np.zeros((iter, 2))
-
     for i in range(iter):
         betaStar, gammaStar = np.array([beta, gamma]) + np.random.normal(size=2)
+        if betaStar<0 or gammaStar<0:
+            i -= 1
+            continue
         if np.random.uniform(0.0, 1.0) < p(betaStar, gammaStar) / p(beta, gamma):
             beta, gamma = betaStar, gammaStar
         samples[i] = np.array([beta, gamma])
@@ -29,10 +29,6 @@ def metropolis_hastings(p, iter=1000):
     return samples
 
 
-samples = metropolis_hastings(circle, iter=10000)
+samples = metropolis_hastings(circle,10000)
 sns.jointplot(samples[:, 0], samples[:, 1])
-
-samples = metropolis_hastings(pgauss, iter=10000)
-sns.jointplot(samples[:, 0], samples[:, 1])
-
 plt.show()
