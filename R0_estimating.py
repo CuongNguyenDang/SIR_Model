8619fa4c2bf2d7fe053a51e814072b6fc132f72a
= <=>
2020-07-20 04:27:02 +0700

    update sampling and implement R0_estimating.py

--- R0_estimating.py
+++ R0_estimating.py
@@ -5,7 +5,9 @@ from matplotlib import pyplot as plt
 import math
 import pylab
 import csv
-
+from sampling import metropolis_hastings
+from sampling import circle
+from sampling import pgauss
 def read_csv(file_name):
     with open(file_name) as csv_file:
         l = []
@@ -20,3 +22,21 @@ def read_csv(file_name):
                     l[i] += int_row[i]
             line_count +=1
     return l
+#to test!!
+file_i ='data/time_series_covid19_confirmed_global.csv'
+file_r = 'data/time_series_covid19_deaths_global.csv'
+###############################
+
+x_i = read_csv(file_i)
+x_r = read_csv(file_r)
+x = [i+j for (i,j) in zip(x_i,x_r)]
+samples = metropolis_hastings(circle,len(x))
+beta = samples[:,0]
+gamma = samples[:,1]
+E_R0 = 0
+for i in range(len(beta)):
+    pi = 1.
+    for val in x:
+        pi *= (gamma[i]**beta[i])/math.gamma(beta[i]) * (val **(beta[i]-1))   *math.exp(-gamma[i]*val)
+    E_R0 += pi *beta[i]/gamma[i]
+print(E_R0)
--- sampling.py
+++ sampling.py
@@ -17,18 +17,15 @@ def pgauss(x, y):
 def metropolis_hastings(p, iter):
     beta, gamma = 0.0212, 3.39 #The starting point of our Markov chain is the estimated value of (α, β) from Raggett (1982)?????
     samples = np.zeros((iter, 2))
-    for i in range(iter):
+    count = 0
+    while count < iter:
         betaStar, gammaStar = np.array([beta, gamma]) + np.random.normal(size=2)
-        if betaStar<0 or gammaStar<0:
-            i -= 1
-            continue
-        if np.random.uniform(0.0, 1.0) < p(betaStar, gammaStar) / p(beta, gamma):
-            beta, gamma = betaStar, gammaStar
-        samples[i] = np.array([beta, gamma])
-
+        if betaStar>0 and gammaStar>0:
+            if np.random.uniform(0.0, 1.0) < p(betaStar, gammaStar) / p(beta, gamma):
+                beta, gamma = betaStar, gammaStar
+                samples[count] = np.array([beta, gamma])
+                count +=1
     return samples
-
-
-samples = metropolis_hastings(circle,10000)
-sns.jointplot(samples[:, 0], samples[:, 1])
-plt.show()
+# samples = metropolis_hastings(circle,10000)
+# sns.jointplot(samples[:, 0], samples[:, 1])
+# plt.show()
