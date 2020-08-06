6ed3963b2c84ce553ccf78abf421497094d30025
binhtranit <binhtran1832@gmail.com>
2020-08-06 22:46:56 +0700

    add loop in R0

--- R0_estimating.py
+++ R0_estimating.py
@@ -12,27 +12,28 @@ from gamma_dist import gamma_dist_exp, f_beta_gamma
 s, i, r = regionize()
 x = np.array([x + y for x, y in zip(i, r) if x + y != 0])
 
-samples = metropolis_hastings(np.array([.1, .025]),
-                              lambda x: f_beta_gamma(x[0], x[1]),
-                              10000)
+for _ in range(20):
+    samples = metropolis_hastings(np.array([.1, .025]),
+                                lambda x: f_beta_gamma(x[0], x[1]),
+                                10000)
 
-beta = np.array(samples[:, 0])
-gamma = np.array(samples[:, 1])
+    beta = np.array(samples[:, 0])
+    gamma = np.array(samples[:, 1])
 
-denom = np.array([sum([gamma_dist_exp(val, b, c) for val in x]) for b, c in zip(beta, gamma)])
-numer = denom + np.array([math.log(b / c) for b, c in zip(beta, gamma)])
+    denom = np.array([sum([gamma_dist_exp(val, b, c) for val in x]) for b, c in zip(beta, gamma)])
+    numer = denom + np.array([math.log(b / c) for b, c in zip(beta, gamma)])
 
-max_numer = np.amax(numer)
-max_denom = np.amax(denom)
+    max_numer = np.amax(numer)
+    max_denom = np.amax(denom)
 
-numer = numer[numer > max_numer - 20]
-denom = denom[denom > max_denom - 20]
+    numer = numer[numer > max_numer - 20]
+    denom = denom[denom > max_denom - 20]
 
-numer -= np.array([max_numer - 10] * numer.shape[0])
-denom -= np.array([max_denom - 10] * denom.shape[0])
+    numer -= np.array([max_numer - 10] * numer.shape[0])
+    denom -= np.array([max_denom - 10] * denom.shape[0])
 
-numer = sum([math.exp(x) for x in numer])
-denom = sum([math.exp(x) for x in denom])
+    numer = sum([math.exp(x) for x in numer])
+    denom = sum([math.exp(x) for x in denom])
 
-R0 = numer / denom * math.exp(max_numer - max_denom)
-print(R0)
+    R0 = numer / denom * math.exp(max_numer - max_denom)
+    print("R0 =", R0)
