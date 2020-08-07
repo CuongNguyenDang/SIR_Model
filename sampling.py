bf0a5286515db850fffcaeef2d0cab58246ce57b
phupfoem <phu.nguyenpfoem@hcmut.edu.vn>
2020-08-08 04:14:16 +0700

    FIXED: grammar errors in docstring

--- sampling.py
+++ sampling.py
@@ -1,14 +1,14 @@
 import numpy as np  # ver 1.19.0
-import scipy.stats as st  # ver 1.5.1
-import seaborn as sns  # ver 0.10.1
-from matplotlib import pyplot as plt  # ver 3.2.2
-from gamma_dist import f_beta_gamma
-import sys
+# import scipy.stats as st  # ver 1.5.1
+# import seaborn as sns  # ver 0.10.1
+# from matplotlib import pyplot as plt  # ver 3.2.2
+# from gamma_dist import f_beta_gamma
+# import sys
 
 
 def metropolis_hastings(x, f, nIter):
     """
-    Metropolis algorithm to draw samples with known probability distribution
+    Metropolis algorithm to draw samples with known probability distribution.
 
     Parameters
     ----------
@@ -21,7 +21,7 @@ def metropolis_hastings(x, f, nIter):
 
     Returns
     ------
-    out:      ndarray with size is nIter x n
+    out:      ndarray with size nIter x n
          Samples with desired probability distribution
     """
     
@@ -44,49 +44,49 @@ def metropolis_hastings(x, f, nIter):
     return samples
 
 
-if __name__ == "__main__":
-    from gamma_dist import gamma_dist_var
+# if __name__ == "__main__":
+#     from gamma_dist import gamma_dist_var
     
-    if len(sys.argv) > 4:
-        # Interpret input
-        lambda_beta, v_beta, lambda_gamma, v_gamma = map(float, sys.argv[1:])
-    else:
-        lambda_beta,v_beta = 0.27,2.47
-        lambda_gamma,v_gamma = 0.19,7.58    
-    # f is proportional to pi(beta, gamma)
-    f = lambda x: gamma_dist_var(x[0], lambda_beta, v_beta) \
-        * gamma_dist_var(x[1], lambda_gamma, v_gamma)
+#     if len(sys.argv) > 4:
+#         # Interpret input
+#         lambda_beta, v_beta, lambda_gamma, v_gamma = map(float, sys.argv[1:])
+#     else:
+#         lambda_beta,v_beta = 0.27,2.47
+#         lambda_gamma,v_gamma = 0.19,7.58    
+#     # f is proportional to pi(beta, gamma)
+#     f = lambda x: gamma_dist_var(x[0], lambda_beta, v_beta) \
+#         * gamma_dist_var(x[1], lambda_gamma, v_gamma)
     
 
-    # Draw samples
-    samples = metropolis_hastings(np.array([lambda_beta / v_beta, lambda_gamma / v_gamma]), f, 100000)
-    beta_samples = samples[:, 0]
-    gamma_samples = samples[:, 1]
+#     # Draw samples
+#     samples = metropolis_hastings(np.array([lambda_beta / v_beta, lambda_gamma / v_gamma]), f, 100000)
+#     beta_samples = samples[:, 0]
+#     gamma_samples = samples[:, 1]
     
-    # Plot
-    h = sns.jointplot(samples[:, 0], samples[:, 1], kind='kde')
-    h.set_axis_labels(r'$\beta$', r'$\gamma$')
-    plt.suptitle('Lấy mẫu bằng Metropolis-Hastings')
+#     # Plot
+#     h = sns.jointplot(samples[:, 0], samples[:, 1], kind='kde')
+#     h.set_axis_labels(r'$\beta$', r'$\gamma$')
+#     plt.suptitle('Lấy mẫu bằng Metropolis-Hastings')
     
-    # Output capture
-    plt.savefig('sampling.png')
+#     # Output capture
+#     plt.savefig('sampling.png')
     
-    # Compare means and variance
-    print('=' * 73)
+#     # Compare means and variance
+#     print('=' * 73)
     
-    print('beta')
-    print('actual : mean: {:.4f}\t variance: {:.4f}'\
-            .format(lambda_beta / v_beta, lambda_beta / v_beta**2))
-    print('samples: mean: {:.4f}\t variance: {:.4f}'\
-            .format(np.mean(beta_samples), np.var(beta_samples)))
+#     print('beta')
+#     print('actual : mean: {:.4f}\t variance: {:.4f}'\
+#             .format(lambda_beta / v_beta, lambda_beta / v_beta**2))
+#     print('samples: mean: {:.4f}\t variance: {:.4f}'\
+#             .format(np.mean(beta_samples), np.var(beta_samples)))
 
-    print('gamma')
-    print('actual : mean: {:.4f}\t variance: {:.4f}'\
-            .format(lambda_gamma / v_gamma, lambda_gamma / v_gamma**2))
-    print('samples: mean: {:.4f}\t variance: {:.4f}'\
-            .format(np.mean(gamma_samples), np.var(gamma_samples)))
+#     print('gamma')
+#     print('actual : mean: {:.4f}\t variance: {:.4f}'\
+#             .format(lambda_gamma / v_gamma, lambda_gamma / v_gamma**2))
+#     print('samples: mean: {:.4f}\t variance: {:.4f}'\
+#             .format(np.mean(gamma_samples), np.var(gamma_samples)))
     
-    print('=' * 73)
+#     print('=' * 73)
 
-    # Show plot
-    plt.show()
+#     # Show plot
+#     plt.show()
