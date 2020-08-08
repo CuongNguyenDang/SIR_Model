e23e88b280c4dba296392e62dd8007424881cd1c
phupfoem <phu.nguyenpfoem@hcmut.edu.vn>
2020-08-08 13:33:01 +0700

    ADDED: RK4 method, MODIFIED: make iterative methods more generic

--- euler.py
+++ euler.py
@@ -2,73 +2,156 @@ import numpy as np
 
 
 def dSIR(s, i, r, beta, gamma, dt):
-    """Define dS(t), dI(t) and dR(t)."""
-    return np.array([-beta / (s + i + r) * i * s,
-                     beta / (s + i + r)  * i * s - gamma * i,
+    """Define dS(t), dI(t) and dR(t) in SIR-epidemic model (SIR for short)."""
+    
+    N = s + i + r
+    return np.array([-beta / N * i * s,
+                     beta / N * i * s - gamma * i,
                      gamma * i]) * dt
 
 
-def euler_step(s0, i0, r0, beta, gamma, dt):
-    """Approximate S(t), I(t), R(t) by Euler method."""
-    return np.array([s0, i0, r0]) + dSIR(s0, i0, r0, beta, gamma, dt)
-
-
-def euler_method(s0, i0, r0, beta, gamma, t):
+def dSIRD(s, i, r, d, beta, gamma, muy, dt):
     """
-    Approximate S, I, R in time range t.
+    Define dS(t), dI(t), dR(t) and dD(t) in SIR-epidemic model
+    with vital dynamics and constant population (SIRD for short).
+    """
+    
+    N = s + i + r + d
+    return np.array([-beta / N * i * s,
+                     beta / N  * i * s - (gamma+muy) * i,
+                     gamma * i,
+                     muy * i]) * dt
+
+
+def euler_step(x0, t0, dx, dt):
+    """
+    Approximate x(1) by Euler method.
+
+    Parameters
+    ----------
+    x0 : tuple of type T
+        Initial value.
+    t0 : float
+        Initial time.
+    dx : callable, (T, float, float) -> T
+        Differential (system of) equation(s).
+    dt : float
+        Time step.
+
+    Returns
+    -------
+    x1 : T
+        Estimated x(1).
+
+    """
+    
+    k1 = dx(x0, t0, dt)
+    
+    x1 = x0 + k1
+    
+    return x1
+
+
+def improved_euler_step(x0, t0, dx, dt):
+    """
+    Approximate x(1) by improved Euler method.
+
+    Parameters
+    ----------
+    x0 : tuple of type T
+        Initial value.
+    t0 : float
+        Initial time.
+    dx : callable, (T, float, float) -> T
+        Differential (system of) equation(s).
+    dt : float
+        Time step.
+
+    Returns
+    -------
+    x1 : T
+        Estimated x(1).
+
+    """
+    
+    k1 = dx(x0, t0, dt)
+    k2 = dx(x0 + k1, t0 + dt, dt)
+    
+    x1 = x0 + (k1+k2) / 2
+    
+    return x1
+
+
+def rk4_step(x0, t0, dx, dt):
+    """
+    Approximate x(1) by fourth-order Runge–Kutta method.
+
+    Parameters
+    ----------
+    x0 : tuple of type T
+        Initial value.
+    t0 : float
+        Initial time.
+    dx : callable, (T, float, float) -> T
+        Differential (system of) equation(s).
+    dt : float
+        Time step.
+
+    Returns
+    -------
+    x1 : T
+        Estimated x(1).
+
+    """
+    
+    k1 = dx(x0, t0, dt)
+    k2 = dx(x0 + k1/2, t0 + dt/2, dt)
+    k3 = dx(x0 + k2/2, t0 + dt/2, dt)
+    k4 = dx(x0 + k3, t0 + dt, dt)
+    
+    x1 = x0 + (k1 + 2*k2 + 2*k3 + k4) / 6
+    
+    return x1
+
+
+def iterative_method(x0, dx, t, method='Euler'):
+    """
+    Approximate x in time range t by iterative method.
     
     Parameters
     ----------
-    s0:      float
-        Initial number of susceptible individuals, S(0)
-    i0:      float
-        Initial number of infectious individuals, I(0)
-    r0:      float
-        Initial number of recovered individuals, R(0)
-    beta:    float
-        Average number of contacts per person per time
-    gamma:   float
-        Probability of an infectious individual recovering
-    t:       array of float with size n
-        Concerned points in time
-
+    x0 : tuple of type T
+        Initial value.
+    dx : callable, (T, float, float) -> T
+        Differential (system of) equation(s).
+    t : array of float with size n
+        Concerned points in time.
+    method : string
+        Iterative method to use.
+        Can recognize 'Euler', 'Improved Euler' and 'Rk4'
+        The default is 'Euler'.
+        
     Returns
     ------
-    s:       ndarray with size n
-        Estimated S values at all concerned points in time
-    i:       ndarray with size n
-        Estimated S values at all concerned points in time
-    r:       ndarray with size n
-        Estimated S values at all concerned points in time
+    x : ndarray of T with size n
+        Estimated x values at all concerned points in time.
+    
     """
     
-    s = [s0]
-    i = [i0]
-    r = [r0]
-    for idx in range(1, len(t)):
-        s0, i0, r0 = euler_step(s0, i0, r0, beta, gamma, t[idx] - t[idx - 1])
-        s.append(s0)
-        i.append(i0)
-        r.append(r0)
+    method = method.title()
+    
+    if method == 'Euler':
+        step_func = lambda x, t0, t1: euler_step(x, t0, dx, t1-t0)
+    elif method == 'Improved Euler':
+        step_func = lambda x, t0, t1: improved_euler_step(x, t0, dx, t1-t0)
+    elif method == 'Rk4':
+        step_func = lambda x, t0, t1: rk4_step(x, t0, dx, t1-t0)
+    else:
+        step_func = lambda x, t0, t1: euler_step(x, t0, dx, t1-t0)
+    
+    x = [x0]
+    for t0, t1 in zip(t[:-1], t[1:]):
+        x0 = step_func(x0, t0, t1)
+        x.append(x0)
 
-    return np.array(s), np.array(i), np.array(r)
-
-
-def improved_euler_step(s0, i0, r0, beta, gamma, dt):
-    """approx i(t), r(t) by improved euler method"""
-    s1, i1, r1 = euler_step(s0, i0, r0, beta, gamma, dt)
-    return np.array([s0, i0, r0]) \
-    + (dSIR(s0, i0, r0, beta, gamma, dt) + dSIR(s1, i1, r1, beta, gamma, dt)) / 2
-
-
-def improved_euler_method(s0, i0, r0, beta, gamma, t):
-    s = [s0]
-    i = [i0]
-    r = [r0]
-    for idx in range(1, len(t)):
-        s0, i0, r0 = improved_euler_step(s0, i0, r0, beta, gamma, t[idx] - t[idx - 1])
-        s.append(s0)
-        i.append(i0)
-        r.append(r0)
-
-    return np.array(s), np.array(i), np.array(r)
+    return np.array(x)
--- plot.py
+++ plot.py
@@ -6,11 +6,11 @@ from pylab import legend
 from datetime import datetime, timedelta
 import sys
 
-from euler import euler_method, improved_euler_method
+from euler import iterative_method, dSIR, dSIRD
 
-def plot_data(s, i, r, t=None, t0=datetime(2020, 1, 22), dt=timedelta(days=1)):
+def plot_ir_data(i, r, t=None, t0=datetime(2020, 1, 22), dt=timedelta(days=1)):
     if t is None:
-        t = drange(t0, t0 + s.shape[0] * dt, dt)
+        t = drange(t0, t0 + i.shape[0] * dt, dt)
     
     fig, ax = plt.subplots() 
     ax.plot_date(t, i, 'r-o', label="Nhiễm bệnh") 
@@ -24,7 +24,24 @@ def plot_data(s, i, r, t=None, t0=datetime(2020, 1, 22), dt=timedelta(days=1)):
     plt.show()
 
 
-def plot_method(s0=796702206,
+def plot_ird_data(i, r, d, t=None, t0=datetime(2020, 1, 22), dt=timedelta(days=1)):
+    if t is None:
+        t = drange(t0, t0 + i.shape[0] * dt, dt)
+    
+    fig, ax = plt.subplots() 
+    ax.plot_date(t, i, 'r-o', label="Nhiễm bệnh") 
+    ax.plot_date(t, r, 'b-x', label="Hồi phục")
+    ax.plot_date(t, d, 'g-*', label="Tử vong")
+    
+    fig.autofmt_xdate()
+    
+    ax.set_title("Giá trị I, R thực")
+    legend(loc='upper left')
+    
+    plt.show()
+
+
+def plot_sir_prediction(s0=796702206,
                 i0=2,
                 r0=0,
                 beta=0.1126,
@@ -33,25 +50,36 @@ def plot_method(s0=796702206,
                 dt=timedelta(days=1),
                 nStep=100,
                 t=None,
-                method="EulerMethod"):
-    if method == "ImprovedEulerMethod":
-        method = improved_euler_method
+                method='Euler'):
+    
+    method = method.title()
+    
+    dsir = lambda x0, t0, dt : dSIR(s=x0[0], i=x0[1], r=x0[2], dt=dt, beta=beta, gamma=gamma)
+    
+    if method == 'Improved Euler':
+        method = lambda s0, i0, r0, t: iterative_method((s0, i0, r0), dsir, t, method='Improved Euler')
         methodName = "Euler mở rộng"
+    elif method == 'Rk4':
+        method = lambda s0, i0, r0, t: iterative_method((s0, i0, r0), dsir, t, method='Rk4')
+        methodName = "Runge-Kutta bậc 4"
     else:
-        method = euler_method
+        method = lambda s0, i0, r0, t: iterative_method((s0, i0, r0), dsir, t, method='Euler')
         methodName = "Euler"
         
     if t is None:
         t = drange(t0, t0 + (nStep + 1) * dt, dt)
     
-    s, i, r = np.rint(method(s0, i0, r0, beta, gamma, t)).astype(int)
+    sir = method(s0, i0, r0, t)
+    print(sir)
+    i = np.rint(sir[:, 1]).astype(int)
+    r = np.rint(sir[:, 2]).astype(int)
     
     print("  {:} {: >15} {: >20} ".format("Ngày","Ca nhiễm","Ca hồi phục"))
     dates = date_range(t0, t0 + nStep * dt, nStep + 1)
     for date, i_t, r_t in zip(dates, i, r):
         print("{:>5} {: >20} {: >20} ".format(date.strftime("%d/%m"), i_t, r_t))
     
-    fig, ax = plt.subplots() 
+    fig, ax = plt.subplots()
     ax.plot_date(t, i, 'r-o', label="Nhiễm bệnh")
     ax.plot_date(t, r, 'b-x', label="Hồi phục")
     
@@ -65,10 +93,10 @@ def plot_method(s0=796702206,
 
 if __name__ == "__main__":
     if len(sys.argv) < 7:
-        plot_method()
+        plot_sir_prediction(method='RK4')
     elif len(sys.argv) == 7:
-        plot_method(s0=sys.argv[1], i0=sys.argv[2], r0=sys.argv[3], \
+        plot_sir_prediction(s0=sys.argv[1], i0=sys.argv[2], r0=sys.argv[3], \
                     beta=sys.argv[4], gamma=sys.argv[5], nStep=sys.argv[6])
     else:
-        plot_method(s0=sys.argv[1], i0=sys.argv[2], r0=sys.argv[3], \
+        plot_sir_prediction(s0=sys.argv[1], i0=sys.argv[2], r0=sys.argv[3], \
             beta=sys.argv[4], gamma=sys.argv[5], nStep=sys.argv[6], method=sys.argv[7])
